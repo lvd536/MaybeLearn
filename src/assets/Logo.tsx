f2e804a32a39ad22622f15a2ed64ca51eb5b8632
lvd. <nbikiti@gmail.com>
2025-11-02 23:26:28 +0400

    - Logo svg to component

---
+++ src/assets/Logo.tsx
@@ -0,0 +1,18 @@
+export default function Logo() {
+    return (
+        <svg
+            width="14"
+            height="14"
+            viewBox="0 0 14 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M-4.06504e-05 8.14199e-05H4.44443V4.44455H8.88883V8.88895H13.3333V13.3334H-4.06504e-05V8.14199e-05Z"
+                fill="white"
+            />
+        </svg>
+    );
+}
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,11 @@
 import { Link } from "react-router-dom";
+import Logo from "../assets/Logo";
 
 export default function NavBar() {
     return (
         <div className="fixed top-0 left-0 px-10 flex items-center justify-between w-screen h-17 border-b border-b-white bg-dark-bg/95">
             <div className="flex items-center gap-4">
-                <svg
-                    width="14"
-                    height="14"
-                    viewBox="0 0 14 14"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M-4.06504e-05 8.14199e-05H4.44443V4.44455H8.88883V8.88895H13.3333V13.3334H-4.06504e-05V8.14199e-05Z"
-                        fill="white"
-                    />
-                </svg>
+                <Logo />
                 <span className="font-bold text-xl">MaybeLearn</span>
             </div>
             <div>
@@ -26,22 +14,18 @@ export default function NavBar() {
                     <li>Tests</li>
                     <li>Rating</li>
                     <div className="flex gap-2">
-                        <li>
-                            <Link
-                                to={"/login"}
-                                className="flex w-21 h-10 font-bold bg-indigo-500 rounded-xl items-center justify-center"
-                            >
-                                Sign up
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                to={"/register"}
-                                className="flex w-21 h-10 font-bold bg-indigo-500/50 rounded-xl items-center justify-center"
-                            >
-                                Log in
-                            </Link>
-                        </li>
+                        <Link
+                            to={"/login"}
+                            className="flex w-21 h-10 font-bold bg-indigo-500 rounded-xl items-center justify-center"
+                        >
+                            Sign up
+                        </Link>
+                        <Link
+                            to={"/register"}
+                            className="flex w-21 h-10 font-bold bg-indigo-500/50 rounded-xl items-center justify-center"
+                        >
+                            Log in
+                        </Link>
                     </div>
                 </ul>
             </div>
